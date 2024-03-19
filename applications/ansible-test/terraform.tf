888659adec112b7221af695d743087319852bf84
Brian Zoetewey <brian.zoetewey@cru.org>
2024-03-19 15:03:58 -0400

    Add ansible-test resources.

---
+++ applications/ansible-test/terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "cru-tf-remote-state"
+    dynamodb_table = "terraform-state-lock"
+    region         = "us-east-1"
+    key            = "applications/ansible-test/terraform.tfstate"
+  }
+  required_providers {
+    ansible = {
+      source  = "ansible/ansible"
+      version = "~> 1.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.30"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Name        = "ansible-test"
+      application = "ansible-test"
+      managed_by  = "terraform"
+      owner       = "devops-engineering-team@cru.org"
+      terraform   = replace(abspath(path.root), "/^.*/(cru-terraform|default)/", "")
+    }
+  }
+}
